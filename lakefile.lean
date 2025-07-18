c73fa06d74a041cf8b46f35f782751c2a6b8df4c
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-07-18 11:14:09 +1000

    Blueprint attempt

--- lakefile.lean
+++ lakefile.lean
@@ -9,3 +9,9 @@ require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "m
 lean_lib QuadraticForms where
   srcDir := "Lean"  -- <--- 🔥 This tells lake where source files live
   globs := #[.submodules `QuadraticForms]
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
